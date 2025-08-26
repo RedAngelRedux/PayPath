9ac952b19caa644edc2482e717f86096ad3de405
Sammy Nava <sammy@sammynava.com>
2025-08-26 10:46:19 -0500

    Add Models; Build Calculator Page

---
+++ Models/Loan.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PayPath.Models;
+
+public class Loan
+{
+    [Required]
+    [Range(1, double.MaxValue, ErrorMessage="Purchase Amount Must Be At Least $1")]
+    public double PurchaseAmount { get; set; }
+
+    [Range(0.0,100,MinimumIsExclusive = true,ErrorMessage ="THe Interest Rate Must be Between 0.00 and 100")]
+    public double Rate { get; set; }
+
+    [Range(1,100,ErrorMessage ="The Term Must Be Between 1 and 100 Years")]
+    public int Term { get; set; }
+
+    public double Payment { get; set; }
+
+    public double TotalInterest { get; set; }
+
+    public double TotalCost { get; set; }
+
+    public List<LoanPayment> AmortizationSchedule { get; set; } = [];
+}
